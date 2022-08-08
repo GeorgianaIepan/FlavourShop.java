91014c1918a25e38b10d2b7f5448dbe2fc5b9988
Nimrod Foldvari <nimrod.foldvari@msg.group>
2022-08-08 15:11:14 +0300

    save order

---
+++ FlavourShop/src/main/java/msg/skillup/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package msg.skillup.repository;
+
+import msg.skillup.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
