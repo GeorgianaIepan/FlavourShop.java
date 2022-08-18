35f8cbafec27119ca547b6d415e88fa2f556fa45
INT\kardab <brigitta.karda@msg.group>
2022-08-18 15:58:26 +0300

    added JWToken Validator

--- FlavourShop/src/main/java/msg/skillup/configuration/SecSecurityConfig.java
+++ FlavourShop/src/main/java/msg/skillup/configuration/SecSecurityConfig.java
@@ -1,6 +1,7 @@
 package msg.skillup.configuration;
 
 import msg.skillup.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @ComponentScan(basePackages = { "msg.skillup" })
 @EnableWebSecurity
 public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -24,6 +30,8 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll();
+        // Add a filter to validate the tokens with every request
+        httpSecurity.addFilterBefore((Filter) jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
