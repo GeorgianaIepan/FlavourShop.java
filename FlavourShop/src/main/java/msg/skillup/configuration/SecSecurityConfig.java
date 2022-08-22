5ab6eca5b95e05cd7b144602bf40a1afedcfb0e0
INT\popr <Raul-Radu.Pop@msg.group>
2022-08-22 09:26:03 +0300

    email verification

--- FlavourShop/src/main/java/msg/skillup/configuration/SecSecurityConfig.java
+++ FlavourShop/src/main/java/msg/skillup/configuration/SecSecurityConfig.java
@@ -5,14 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @ComponentScan(basePackages = { "msg.skillup" })
@@ -29,10 +37,12 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
                 .authorizeRequests()
                 .anyRequest()
-                .permitAll();
-        // Add a filter to validate the tokens with every request
-        httpSecurity.addFilterBefore((Filter) jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+                .permitAll()
+                .and()
+                .addFilterBefore(jwtRequestFilter,UsernamePasswordAuthenticationFilter.class);
+      }
+
+
 
 }
 
