7b3f44612f6817e1092c540d33120bec6314a5c9
INT\cojocv <Vincentiu.Cojocaru@msg.group>
2022-08-25 13:59:25 +0300

    forgot password - backend update

--- FlavourShop/src/main/java/msg/skillup/service/UserService.java
+++ FlavourShop/src/main/java/msg/skillup/service/UserService.java
@@ -2,6 +2,7 @@ package msg.skillup.service;
 
 import msg.skillup.configuration.JWTokenCreator;
 import msg.skillup.converter.UserConverter;
+import msg.skillup.dto.ForgotPasswordDTO;
 import msg.skillup.dto.UserDTO;
 import msg.skillup.exception.BusinessException;
 import msg.skillup.model.Role;
@@ -60,15 +61,15 @@ public class UserService {
         }
     }
 
-    public void resetPassword(Long userId, String password) throws BusinessException{
-        User user = userRepository.getById(userId);
-        user.setPassword(password);
-        UserValidator.errorList.clear();
-        userValidator.validate(user);
-        if (UserValidator.errorList.isEmpty()) {
+    public void resetPassword(ForgotPasswordDTO forgotPasswordDTO) throws BusinessException{
+        User user = userRepository.getById(forgotPasswordDTO.getId());
+        if(!forgotPasswordDTO.getConfirmationPassword().equals(forgotPasswordDTO.getPassword()))
+            throw new BusinessException("passwords do not match");
+        String error = userValidator.validatePassword(user, forgotPasswordDTO.getPassword());
+        if (error == null) {
             userRepository.updatePassword(user.getPassword(), user.getIdUser());
         } else {
-            throw new BusinessException(UserValidator.errorList.toString());
+            throw new BusinessException(error);
         }
 
     }
