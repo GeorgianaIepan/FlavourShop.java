18c44cbba25a5a57903b359722f1b9c3db81bc77
INT\lucaca1 <Andrada.Lucaciu@msg.group>
2022-08-25 12:30:49 +0300

    products db - backend

--- FlavourShop/src/main/resources/import.sql
+++ FlavourShop/src/main/resources/import.sql
@@ -11,6 +11,15 @@ insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) val
 insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (10, 'Hand cream', 100, 'Keep your hands dry, sensitive, soft, velvety and protected against dryness with our intensely moisturizing Flavor Hand Balm. Our balm cares for nails and cuticles and nourishes dry, sensitive skin with 96 hours of hydration: 100 ml');
 insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (20, 'Body spray', 100, 'Smell irresistible with our Flavor body spray, with an improved formula. Our spray leaves the skin instantly refreshed and smelling of ripe mango from head to toe: 250 ml');
 
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (30, 'Body butter', 100, 'Love, soothe and nourish dry, sensitive skin like never before with the best nourishing Flavor body butter ever. It leaves the skin feeling smoother, smoother and nourished with 96 hours of hydration. It really gives the skin a natural-looking glow: 1000 ml');
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (25, 'Hair balm', 100, 'Restore the shine of dull hair with the Flavor conditioner for shine and protection. Our conditioner leaves dull hair looking shiny and more radiant, protecting it against daily pollution: 750 ml');
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (35, 'Body oil', 100, 'Provides intense hydration to very dry skin with our Flavor nourishing body oil. Spray a thin layer of body oil. Quickly absorbed and non-sticky, it leaves gray skin radiant with a natural,healthy-looking glow. Flavor oil helps protect the skins natural hydration barrier for up to 24 hours, intensely nourishing skin that feels smoother and more supple: 500 ml');
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (12, 'Bath bomb', 100, 'Pamper yourself with our Flavor aromas and color your bathroom: 200 g');
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (15, 'Lip balm', 100, 'Moisturize the lips in a single application with our new balm intensely hydrating Flavour. Apply our on-the-go lip savior for lips that feel soft and hydrated with a subtle shine. Reapply whenever you need hydration for healthy-looking lips throughout the day: 20 g');
+insert into PRODUCT(PRICE_PRODUCT, NAME_PRODUCT, STOCK_PRODUCT, DESCRIPTION) values (40, 'Body scrub', 100, 'Gently exfoliate that wonderful body with our Flavor scrub. Made with ingredients of natural origin, the creamy scrub removes dead skin cells leaving it feeling soft and smooth: 750 g');
+
+
+
 
 insert into INGREDIENT(PRICE_INGREDIENT, NAME_INGREDIENT) values (5, 'Coconut');
 insert into INGREDIENT(PRICE_INGREDIENT, NAME_INGREDIENT) values (5, 'Olive');
